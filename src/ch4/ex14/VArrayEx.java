50bec778844127ab0b0dcaf4b5ef9da1730c4635
yejoo0310 <yejoo0310@naver.com>
2026-07-24 00:50:54 +0900

    refactor: VArray 용량 관리 로직 개선

--- src/ch4/ex14/VArrayEx.java
+++ src/ch4/ex14/VArrayEx.java
@@ -1,47 +1,46 @@
 package src.ch4.ex14;
-class VArray{
-    private int capacity;
+
+class VArray {
     private int size;
     private int[] array;
 
-    public VArray(int capacity){
-        this.capacity = capacity;
+    public VArray(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException(
+                    "초기 용량은 1 이상이어야 합니다.");
+        }
         this.size = 0;
-        array = new int[this.capacity];
+        array = new int[capacity];
     }
 
-    public int capacity(){
-        return capacity;
+    public int capacity() {
+        return array.length;
     }
 
-    public void setCapacity(){
-        this.capacity *= 2;
-    }
-
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printAll(){
-        for (int i = 0; i < size; i++){
+    public void printAll() {
+        for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
     }
 
-    public void add(int num){
-        if (size >= capacity){
-            initialize();
+    public void add(int num) {
+        if (size >= array.length) {
+            resizeArray();
         }
         array[size] = num;
         size += 1;
     }
 
-    public void insert(int index, int num){
-        if (size >= capacity){
-            initialize();
+    public void insert(int index, int num) {
+        if (size >= array.length) {
+            resizeArray();
         }
-        for (int i = index; i <= size; i++){
+        for (int i = index; i <= size; i++) {
             int tmp = array[i];
             array[i] = num;
             num = tmp;
@@ -49,33 +48,32 @@ class VArray{
         size += 1;
     }
 
-    public void remove(int index){
+    public void remove(int index) {
         if (index > size) {
             return;
         }
-        for (int i = index; i < size; i++){
+        for (int i = index; i < size; i++) {
             array[i] = array[i + 1];
         }
         size -= 1;
         array[size] = 0;
     }
 
-    public void initialize(){
-        int[] newArray = new int[capacity * 2];
-        for (int i = 0; i < array.length; i++){
+    private void resizeArray() {
+        int[] newArray = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
         }
-        setCapacity();
         array = newArray;
     }
 }
 
 public class VArrayEx {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         VArray v = new VArray(5);
         System.out.println("용량: " + v.capacity() + ", 저장된 개수: " + v.size());
 
-        for (int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++) {
             v.add(i);
         }
         System.out.println("용량: " + v.capacity() + ", 저장된 개수: " + v.size());
@@ -90,7 +88,7 @@ public class VArrayEx {
         System.out.println("용량: " + v.capacity() + ", 저장된 개수: " + v.size());
         v.printAll();
 
-        for(int i = 50; i < 55; i++){
+        for (int i = 50; i < 55; i++) {
             v.add(i);
         }
         System.out.println("용량: " + v.capacity() + ", 저장된 개수: " + v.size());
