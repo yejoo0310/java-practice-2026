20a64d1ccaa06b36a74ebc125c6b356eba0e420f
yejoo0310 <yejoo0310@naver.com>
2026-07-27 01:16:02 +0900

    refactor: 클래스명 및 파일명 변경

---
+++ src/ch5/ex03/ColorPoint.java
@@ -0,0 +1,56 @@
+package src.ch5.ex03;
+
+class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    protected void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class ColorPoint extends Point {
+    private String color;
+
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    protected void setXY(int x, int y) {
+        move(x, y);
+    }
+
+    protected void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String toString() {
+        return getColor() + "색의 (" + getX() + "," + getY() + ")의 점";
+    }
+
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint(5, 5, "RED");
+        cp.setXY(10, 20);
+        cp.setColor("BLUE");
+        String str = cp.toString();
+        System.out.println(str + "입니다.");
+    }
+}
