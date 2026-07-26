b951819ef2a87178bce948f23e9196077bf3389f
yejoo0310 <yejoo0310@naver.com>
2026-07-27 01:25:29 +0900

    refactor: Point 클래스 분리

--- src/ch5/ex03/ColorPoint.java
+++ src/ch5/ex03/ColorPoint.java
@@ -1,27 +1,5 @@
 package src.ch5.ex03;
 
-class Point {
-    private int x, y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    protected void move(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class ColorPoint extends Point {
     private String color;
 
