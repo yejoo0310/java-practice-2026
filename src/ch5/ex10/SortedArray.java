4cc26f672ea5350f0b8885a124a9663a95aa3a8b
yejoo0310 <yejoo0310@naver.com>
2026-07-27 03:11:28 +0900

    feat: 삽입정렬로 add 메소드 오버라이딩

--- src/ch5/ex10/SortedArray.java
+++ src/ch5/ex10/SortedArray.java
@@ -1,23 +1,37 @@
 package src.ch5.ex10;
+
 import java.util.Scanner;
 import src.ch5.ex09.BaseArray;
 
-public class SortedArray extends BaseArray{
-    public SortedArray(int size){
+public class SortedArray extends BaseArray {
+    public SortedArray(int size) {
         super(size);
     }
 
-    public void add(int n){
+    @Override
+    public void add(int n) {
+        if (nextIndex == array.length) {
+            return;
+        }
+
         super.add(n);
-        
+
+        // 삽입정렬
+        int i = nextIndex - 2;
+        while (i >= 0 && array[i] > n) {
+            array[i + 1] = array[i];
+            i--;
+        }
+
+        array[i + 1] = n;
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         SortedArray sArray = new SortedArray(10);
-    
+
         Scanner scanner = new Scanner(System.in);
         System.out.print(">>");
-        for (int i = 0; i < sArray.length(); i++){
+        for (int i = 0; i < sArray.length(); i++) {
             int n = scanner.nextInt();
             sArray.add(n);
         }
