44fd5fd27c5341af3cf9c1fbb38637af94ee2552
yejoo0310 <yejoo0310@naver.com>
2026-07-27 09:22:49 +0900

    feat: delete 기능 구현 및 put 메소드에서 NullPointerException 에러 수정

--- src/ch5/ex12/DictionaryApp.java
+++ src/ch5/ex12/DictionaryApp.java
@@ -1,69 +1,85 @@
 package src.ch5.ex12;
 
-import java.util.*;
-
-abstract class PairMap{
+abstract class PairMap {
     protected String keyArray[];
     protected String valueArray[];
+
     abstract public String get(String key);
+
     abstract public void put(String key, String value);
+
     abstract public String delete(String key);
+
     abstract public int length();
 }
 
-class Dictionary extends PairMap{
+class Dictionary extends PairMap {
     private int size;
-    private int amount;
-    public Dictionary(int size){
+    private int length;
+
+    public Dictionary(int size) {
         this.size = size;
-        this.amount = 0;
+        this.length = 0;
         keyArray = new String[this.size];
         valueArray = new String[this.size];
     }
 
-    public String get(String key){
-        for (int i = 0; i < keyArray.length; i++){
-            if (keyArray[i].equals(key)){
+    @Override
+    public String get(String key) {
+        for (int i = 0; i < length; i++) {
+            if (key.equals(keyArray[i])) {
                 return valueArray[i];
             }
-        }   
+        }
         return null;
     }
 
-    public void put(String key, String value){
-        if (amount == size){
-            return;
-        }
-        if (get(key).equals("null")){
-            keyArray[amount] = key;
-            valueArray[amount] = value;
-            amount++;
-            return;
-        }
-        for (int i = 0; i < keyArray.length; i++){
-            if (keyArray[i].equals(key)){
+    @Override
+    public void put(String key, String value) {
+        for (int i = 0; i < length; i++) {
+            if (key.equals(keyArray[i])) {
                 valueArray[i] = value;
                 return;
             }
         }
+
+        if (length == size) {
+            return;
+        }
+
+        keyArray[length] = key;
+        valueArray[length] = value;
+        length++;
     }
 
-    public String delete(String key){
-        for (int i = 0; i < keyArray.length; i++){
-            if (keyArray[i].equals(key)){
-                keyArray[i] = null;
-                valueArray[i] = null;
-                for (int j = i; j < keyArray.length; j++){
-                    String tmpKey = keyArray[j]
+    @Override
+    public String delete(String key) {
+        for (int i = 0; i < length; i++) {
+            if (key.equals(keyArray[i])) {
+                String deletedValue = valueArray[i];
+
+                for (int j = i; j < length - 1; j++) {
+                    keyArray[j] = keyArray[j + 1];
+                    valueArray[j] = valueArray[j + 1];
                 }
-                amount--;
+
+                length--;
+                keyArray[length] = null;
+                valueArray[length] = null;
+                return deletedValue;
             }
         }
+        return null;
+    }
+
+    @Override
+    public int length() {
+        return length;
     }
 }
 
 public class DictionaryApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Dictionary dic = new Dictionary(10);
         dic.put("황기태", "자바");
         dic.put("이재문", "파이썬");
