f6b4e2a672813f7e268c8f01bbd012f01f20350f
yejoo0310 <yejoo0310@naver.com>
2026-07-27 15:49:00 +0900

    feat: 영문자 히스토그램 구현

---
+++ src/ch6/openchallenge/AlphabetHistogramApp.java
@@ -0,0 +1,69 @@
+package src.ch6.openchallenge;
+
+import java.util.Scanner;
+import java.lang.StringBuffer;
+
+public class AlphabetHistogramApp {
+    private final Scanner scanner;
+    private String str;
+    private int[] count;
+
+    public AlphabetHistogramApp() {
+        scanner = new Scanner(System.in);
+        count = new int[26];
+    }
+
+    private String readString() {
+        StringBuffer sb = new StringBuffer();
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equals(";")) {
+                break;
+            }
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private void calculate(int number) {
+        char alphabet = (char) ('A' + number);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == alphabet) {
+                count[number]++;
+            }
+        }
+    }
+
+    private void printHistogram() {
+        for (int i = 0; i < count.length; i++) {
+            char alphabet = (char) ('A' + i);
+            System.out.print(alphabet);
+            for (int j = 0; j < count[i]; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+
+    public void run() {
+        str = readString().toUpperCase();
+        for (int i = 0; i < count.length; i++) {
+            calculate(i);
+        }
+        printHistogram();
+    }
+
+    public void close() {
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        AlphabetHistogramApp app = new AlphabetHistogramApp();
+
+        try {
+            app.run();
+        } finally {
+            app.close();
+        }
+    }
+}
