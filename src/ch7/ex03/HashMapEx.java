f9fa65813799a459fe400292a85b459facca7e21
yejoo0310 <yejoo0310@naver.com>
2026-07-28 14:31:31 +0900

    pref: HashMap의 containsKey 메소드를 사용해 검색 기능 개선

--- src/ch7/ex03/HashMapEx.java
+++ src/ch7/ex03/HashMapEx.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class HashMapEx {
     private final Scanner scanner;
-    private final HashMap<String, Integer> stocks;
+    private final Map<String, Integer> stocks;
 
     public HashMapEx(Scanner scanner) {
         this.scanner = scanner;
@@ -17,63 +17,60 @@ public class HashMapEx {
         while (true) {
             System.out.print("종목, 주가>>");
             String line = scanner.nextLine().trim();
+
+            if (line.isEmpty()) {
+                System.out.println("종목과 주가를 입력해주세요.");
+                continue;
+            }
+
+            if (line.equals("그만")) {
+                break;
+            }
+
             String inputs[] = line.split("\\s+");
             if (inputs.length != 2) {
-                if (inputs[0].equals("그만")) {
-                    break;
-                }
                 System.out.println("종목과 주가를 빈 칸 기준으로 작성해야 합니다.");
                 continue;
             }
+
             String name = inputs[0];
             try {
                 int price = Integer.parseInt(inputs[1]);
                 stocks.put(name, price);
             } catch (NumberFormatException e) {
                 System.out.println("주가는 정수로 입력해야 합니다.");
-                continue;
             }
         }
     }
 
-    private boolean isExist(String input) {
-        Set<String> keys = stocks.keySet();
-        Iterator<String> it = keys.iterator();
-
-        while (it.hasNext()) {
-            String name = it.next();
-            if (name.equals(input)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void searchStock() {
+    private void searchStocks() {
         System.out.println("주가를 검색합니다.");
 
         while (true) {
             System.out.print("주가>>");
             String name = scanner.nextLine().trim();
+
             if (name.isEmpty()) {
                 System.out.println("종목을 검색해주세요.");
                 continue;
             }
+
             if (name.equals("그만")) {
                 return;
             }
-            if (isExist(name)) {
+
+            if (stocks.containsKey(name)) {
                 int price = stocks.get(name);
                 System.out.println(name + "의 주가는 " + price + "원");
-                continue;
+            } else {
+                System.out.println(name + "은(는) 없는 종목입니다.");
             }
-            System.out.println(name + "은(는) 없는 종목입니다.");
         }
     }
 
     public void run() {
         readStocks();
-        searchStock();
+        searchStocks();
     }
 
     public static void main(String[] args) {
